cf4a0433f8fb7e498e594d9e780ed35dfc88e288
Sorque <michvl@yandex.ru>
2023-04-19 22:00:00 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by m.korovin on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
